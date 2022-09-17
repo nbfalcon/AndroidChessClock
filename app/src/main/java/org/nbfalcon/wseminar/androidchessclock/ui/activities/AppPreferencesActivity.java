08ad0e06b23221aee746450f04109e7d13b65cf1
Nikita Bloshchanevich <nikblos@outlook.com>
2022-09-17 22:23:50 +0200

    Oss license notice plugin
    Skip material design icons, attribution is apparently not required and OssLicenseMenuActivity is too inflexible

--- app/src/main/java/org/nbfalcon/wseminar/androidchessclock/ui/activities/AppPreferencesActivity.java
+++ app/src/main/java/org/nbfalcon/wseminar/androidchessclock/ui/activities/AppPreferencesActivity.java
@@ -1,11 +1,16 @@
 package org.nbfalcon.wseminar.androidchessclock.ui.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import org.nbfalcon.wseminar.androidchessclock.R;
 import org.nbfalcon.wseminar.androidchessclock.util.android.activity.SettingsActivityBase;
+
+import java.util.Objects;
 
 public class AppPreferencesActivity extends SettingsActivityBase {
     @Override
@@ -26,6 +31,17 @@ public class AppPreferencesActivity extends SettingsActivityBase {
 
     // public needed so that the App doesn't crash
     public static class AppPreferencesFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            Preference ossLicensesButton = findPreference("ossLicensesButton");
+            Objects.requireNonNull(ossLicensesButton).setOnPreferenceClickListener(preference -> {
+                startActivity(new Intent(getActivity(), OssLicensesMenuActivity.class));
+                return true;
+            });
+        }
+
         @Override
         public void onCreatePreferences(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState, @Nullable @org.jetbrains.annotations.Nullable String rootKey) {
             setPreferencesFromResource(R.xml.app_preferences, rootKey);
